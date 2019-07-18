8f03e53a7e8892568d7034d47f278774f094b070
Huy Ha <huyhnc81@gmail.com>
2019-07-18 16:38:51 +0700

    Add angle control

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,49 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject Bullet;
     public float ForcePerSecondSpaceHold;
+    public float AnglePerSecondControlling;
     public float Speed;
 
     private float Angle;
-    private float DeltaTime;
+    private float DeltaTimeBullet;
+    private float DeltaTimeAngle;
+    private float AngleMul;
     private Rigidbody2D PlayerRB2D;
     // Start is called before the first frame update
     void Start()
     {
         PlayerRB2D = GetComponent<Rigidbody2D>();
         Angle = 45;
-        DeltaTime = 0;
+        AngleMul = 1;
+        DeltaTimeBullet = 0;
         RaiseFriction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            LowFriction();
-            PlayerRB2D.velocity = new Vector2(Speed, PlayerRB2D.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            LowFriction();
-            PlayerRB2D.velocity = new Vector2(-Speed, PlayerRB2D.velocity.y);
-        }
-        else if(Input.GetKey(KeyCode.W))
-        {
-            Angle++;
-            print(Angle);
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            Angle--;
-            print(Angle);
-        }
-        else if(Input.GetKey(KeyCode.Space))
-        {
-            DeltaTime += Time.deltaTime;
-            print(DeltaTime);
-        }
-
         if (Input.GetKeyDown(KeyCode.D))
         {
             LowFriction();
@@ -59,25 +37,75 @@ public class PlayerController : MonoBehaviour
             LowFriction();
             PlayerRB2D.velocity = new Vector2(-Speed, PlayerRB2D.velocity.y);
         }
+        
 
+        if (Input.GetKey(KeyCode.D))
+        {
+            LowFriction();
+            PlayerRB2D.velocity = new Vector2(Speed, PlayerRB2D.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            LowFriction();
+            PlayerRB2D.velocity = new Vector2(-Speed, PlayerRB2D.velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            DeltaTimeBullet += Time.deltaTime;
+            print(DeltaTimeBullet);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            DeltaTimeAngle += Time.deltaTime;
+            Angle += (Time.deltaTime * AnglePerSecondControlling) * AngleMul;
+            AngleMul += 0.03f;
+            print(Angle);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            DeltaTimeAngle += Time.deltaTime;
+            Angle -= (Time.deltaTime * AnglePerSecondControlling) * AngleMul;
+            AngleMul += 0.03f;
+            print(Angle);
+        }
+        
         if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
         {
             RaiseFriction();
             PlayerRB2D.velocity = new Vector2(0, 0);
         }
 
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (DeltaTime != 0)
+            if (DeltaTimeBullet != 0)
             {
                 GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
-                float force = DeltaTime * ForcePerSecondSpaceHold;
+                float force = DeltaTimeBullet * ForcePerSecondSpaceHold;
                 float angleInRadian = Mathf.Deg2Rad * Angle;
                 bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(force * Mathf.Cos(angleInRadian),
                                                                         force * Mathf.Sin(angleInRadian)));
-                DeltaTime = 0;
+                DeltaTimeBullet = 0;
             }
         }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (DeltaTimeAngle * AnglePerSecondControlling < 1)
+                Angle = Angle - DeltaTimeAngle * AnglePerSecondControlling + 1;
+            DeltaTimeAngle = 0;
+            AngleMul = 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (DeltaTimeAngle * AnglePerSecondControlling < 1)
+                Angle = Angle + DeltaTimeAngle * AnglePerSecondControlling - 1;
+            DeltaTimeAngle = 0;
+            AngleMul = 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
     }
     void RaiseFriction()
     {

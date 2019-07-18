b276537e209792faf8e005ef41e0e1a179b35d33
Huy Ha <huyhnc81@gmail.com>
2019-07-18 11:13:20 +0700

    Add shooting

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject Bullet;
-
+    public float ForcePerSecondSpaceHold;
     public float Speed;
+
+    private float Angle;
+    private float DeltaTime;
     private Rigidbody2D PlayerRB2D;
     // Start is called before the first frame update
     void Start()
     {
         PlayerRB2D = GetComponent<Rigidbody2D>();
+        Angle = 45;
+        DeltaTime = 0;
         RaiseFriction();
     }
 
@@ -28,11 +33,31 @@ public class PlayerController : MonoBehaviour
             LowFriction();
             PlayerRB2D.velocity = new Vector2(-Speed, PlayerRB2D.velocity.y);
         }
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        else if(Input.GetKey(KeyCode.W))
         {
-            GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(450,450));
+            Angle++;
+            print(Angle);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            Angle--;
+            print(Angle);
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            DeltaTime += Time.deltaTime;
+            print(DeltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            LowFriction();
+            PlayerRB2D.velocity = new Vector2(Speed, PlayerRB2D.velocity.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            LowFriction();
+            PlayerRB2D.velocity = new Vector2(-Speed, PlayerRB2D.velocity.y);
         }
 
         if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
@@ -41,6 +66,18 @@ public class PlayerController : MonoBehaviour
             PlayerRB2D.velocity = new Vector2(0, 0);
         }
 
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if (DeltaTime != 0)
+            {
+                GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+                float force = DeltaTime * ForcePerSecondSpaceHold;
+                float angleInRadian = Mathf.Deg2Rad * Angle;
+                bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(force * Mathf.Cos(angleInRadian),
+                                                                        force * Mathf.Sin(angleInRadian)));
+                DeltaTime = 0;
+            }
+        }
     }
     void RaiseFriction()
     {

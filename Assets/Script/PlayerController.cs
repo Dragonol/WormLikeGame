ad6aace1e19e15d6e73f4a6ab4d276c49a898760
Huy Ha <huyhnc81@gmail.com>
2019-08-10 00:16:59 +0700

    update movement

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -12,11 +12,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D PlayerRB2D;
     private CircleCollider2D CircleCollider2D;
 
+    private LayerMask TerrainLayer;
     
     void Start()
     {
         PlayerRB2D = GetComponent<Rigidbody2D>();
         CircleCollider2D = GetComponent<CircleCollider2D>();
+
+        TerrainLayer = LayerMask.GetMask("Terrain");
 
         Grounded = false;
     }
@@ -24,6 +27,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateGrounded();
+
         // Moving horizontal
         if (Input.GetKey(KeyCode.D))
         {
@@ -41,15 +46,17 @@ public class PlayerController : MonoBehaviour
             Stop();
             PlayerRB2D.velocity = new Vector2(0, PlayerRB2D.velocity.y);
         }
-    }
 
-    private void FixedUpdate()
-    {
-        UpdateGrounded();
+        // Moving vertical
         if (Input.GetKey(KeyCode.W) && Grounded)
         {
             PlayerRB2D.AddForce(new Vector2(0, JumpForce));
         }
+    }
+
+    private void FixedUpdate()
+    {
+
     }
     
     private void Stop()
@@ -62,17 +69,10 @@ public class PlayerController : MonoBehaviour
     }
     private void UpdateGrounded()
     {
-        LayerMask layer = LayerMask.GetMask("Terrain");
-        //var currPos = CircleCollider2D.bounds.center;
-        //var colliderRadius = CircleCollider2D.radius;
-        //float dist = 0.01f;
-        //var ray1 = Physics2D.Raycast(currPos, new Vector2(0, -1), colliderRadius + dist, layer).collider;
-        //var ray2 = Physics2D.Raycast(new Vector2(currPos.x - (colliderRadius/2), currPos.y), new Vector2(0, -1), Mathf.Sqrt(3) * colliderRadius/2 + dist, layer).collider;
-        //var ray3 = Physics2D.Raycast(new Vector2(currPos.x + (colliderRadius/2), currPos.y), new Vector2(0, -1), Mathf.Sqrt(3) * colliderRadius / 2 + dist, layer).collider;
+        Grounded = CircleCollider2D.IsTouchingLayers(TerrainLayer);
 
-        //Grounded = ray1 || ray2 || ray3;
-
-        if ((Grounded = CircleCollider2D.IsTouchingLayers(layer)))
+        // Set velocity to zero when touch ground
+        if (Grounded)
             PlayerRB2D.velocity = new Vector2(PlayerRB2D.velocity.x, 0);
     }
 }
